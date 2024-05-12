3c185f4dfe42321aa9fc366ab004b17df9e8e4de
Ankit Kumar <hiankit@proton.me>
2024-05-12 16:21:20 +0530

    few changes in add expense page

--- src/screens/AddExpense.jsx
+++ src/screens/AddExpense.jsx
@@ -14,88 +14,83 @@ const AddExpense = () => {
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState({
     value: '',
-    list: [
-      {_id: 1, value: 'Food & Dining'},
-      {_id: 2, value: 'Groceries'},
-      {_id: 3, value: 'Transportation'},
-      {_id: 4, value: 'Utilities'},
-      {_id: 5, value: 'Rent/Mortgage'},
-      {_id: 6, value: 'Insurance'},
-      {_id: 7, value: 'Entertainment'},
-      {_id: 8, value: 'Shopping'},
-      {_id: 9, value: 'Health & Fitness'},
-      {_id: 10, value: 'Education'},
-      {_id: 11, value: 'Travel'},
-      {_id: 12, value: 'Gifts & Donations'},
-      {_id: 13, value: 'Personal Care'},
-      {_id: 14, value: 'Pet Expenses'},
-      {_id: 15, value: 'Taxes'},
-      {_id: 16, value: 'Subscriptions'},
-      {_id: 17, value: 'Home Maintenance'},
-      {_id: 18, value: 'Miscellaneous'},
-    ],
     selectedList: [],
     error: '',
   });
-
-  console.log([...category.list]);
-
+  const categories = [
+    {_id: 1, value: 'Food & Dining'},
+    {_id: 2, value: 'Groceries'},
+    {_id: 3, value: 'Transportation'},
+    {_id: 4, value: 'Utilities'},
+    {_id: 5, value: 'Rent/Mortgage'},
+  ];
   return (
     <View>
       <AppHeader title="Add Expense" />
-      <View>
-        <TextInput
-          label="Expense Name"
-          value={name}
-          onChangeText={name => setName(name)}
-          mode="outlined"
-        />
-      </View>
-      <View style={styles.colMargin}>
-        <TextInput
-          label="Amount"
-          value={amount}
-          onChangeText={amount => setAmount(amount)}
-          mode="outlined"
-        />
-      </View>
-      <PaperSelect
-        label="Select Category"
-        value={category.value}
-        onSelection={value => {
-          setCategory({
-            ...category,
-            value: value.text,
-            selectedList: value.selectedList,
-            error: '',
-          });
-        }}
-        arrayList={[...category.list]}
-        selectedArrayList={category.selectedList}
-        errorText={category.error}
-        multiEnable={false}
-        hideSearchBox={true}
-        textInputMode="outlined"
-        textInputOutlineStyle={{borderColor: theme.colors.outline}}
-      />
-      <View>
-        <Button
-          mode="contained"
-          onPress={() => navigation.push('Home')}
-          textColor="#DAE0E2"
-          buttonColor="#2F363F"
-          theme={{roundness: 1}}
-          compact={false}>
-          <Text variant="titleMedium" style={styles.buttonText}>
-            SignUp Now
-          </Text>
-        </Button>
+      <View style={styles.container}>
+        <View style={styles.inputFieldMargin}>
+          <TextInput
+            label="Expense Name"
+            value={name}
+            onChangeText={name => setName(name)}
+            mode="outlined"
+          />
+        </View>
+        <View style={styles.inputFieldMargin}>
+          <TextInput
+            label="Amount"
+            value={amount}
+            onChangeText={amount => setAmount(amount)}
+            mode="outlined"
+          />
+        </View>
+        <View style={styles.inputFieldMargin}>
+          <PaperSelect
+            label="Select Category"
+            value={category.value}
+            onSelection={value => {
+              setCategory({
+                ...category,
+                value: value.text,
+                selectedList: value.selectedList,
+                error: '',
+              });
+            }}
+            arrayList={[...categories]}
+            selectedArrayList={category.selectedList}
+            errorText={category.error}
+            multiEnable={false}
+            hideSearchBox={true}
+            textInputMode="outlined"
+            textInputOutlineStyle={{borderColor: theme.colors.outline}}
+          />
+        </View>
+        <View style={styles.inputFieldMargin}>
+          <Button
+            mode="contained"
+            onPress={() => navigation.push('Home')}
+            textColor="#DAE0E2"
+            buttonColor="#2F363F"
+            theme={{roundness: 1}}
+            compact={false}>
+            <Text variant="titleLarge" style={styles.buttonText}>
+              Add Now
+            </Text>
+          </Button>
+        </View>
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  inputFieldMargin: {
+    marginTop: 10,
+  },
+});
 
 export default AddExpense;
 
